06a821695934a0009ec0f23ad0a2f63c8c8d5b08
KI JONG BYUN <kjbyun0@gmail.com>
2024-03-11 16:18:31 -0500

    Assignment Done

--- server/app.py
+++ server/app.py
@@ -24,6 +24,7 @@ class ClearSession(Resource):
     
         session['page_views'] = None
         session['user_id'] = None
+        print('session is cleared')
 
         return {}, 204
 
@@ -47,10 +48,40 @@ class ShowArticle(Resource):
             return make_response(article_json, 200)
 
         return {'message': 'Maximum pageview limit reached'}, 401
+    
+class Login(Resource):
+    def post(self):
+        username = request.get_json()['username']
+        user = User.query.filter_by(username=username).first()
+        if user:
+            session['user_id'] = user.id
+            return make_response(user.to_dict(), 200)
+        return make_response(
+            {'message': f"User {username} isn't valid"},
+            401,
+        )
+
+class Logout(Resource):
+    def delete(self):
+        session['user_id'] = None
+        return make_response({}, 204)
+
+class CheckSession(Resource):
+    def get(self):
+        user_id = session.get('user_id')
+        print(f'user_id: {user_id}')
+        user = User.query.filter_by(id=user_id).first()
+        print(f'user: {user}')
+        if user:
+            return make_response(user.to_dict(), 200)
+        return make_response({}, 401)
 
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
+api.add_resource(CheckSession, '/check_session')
 
 
 if __name__ == '__main__':
